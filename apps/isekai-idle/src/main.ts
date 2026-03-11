40defbb26dc828db165bc3da3c749bb11a5edc09
Dawid Kozak <dkz@spyro-soft.com>
2026-03-11 11:47:41 +0100

    feat: add base for new game

---
+++ apps/isekai-idle/src/main.ts
@@ -0,0 +1,25 @@
+import './styles.scss';
+import router from './router';
+import { createApp } from 'vue';
+import App from './app/App.vue';
+import { createPinia } from 'pinia';
+
+// Vuetify
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(pinia);
+app.use(vuetify);
+app.use(router);
+app.mount('#root');
